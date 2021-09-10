6ca811ab73e43610238f1969f66f0b44ffd857ef
ngvuong <vminguyen@ucdavis.edu>
2021-09-10 00:37:00 -0700

    Update add task form markup

--- src/today-view.js
+++ src/today-view.js
@@ -4,12 +4,16 @@ export function todayView() {
   const heading = document.createElement("h1");
   heading.textContent = "Today's Tasks";
 
-  const addTaskBtn = document.createElement("button");
-  addTaskBtn.textContent = "Add New Task";
+  const btn = document.createElement("button");
+  btn.textContent = "Add New Task";
+  btn.classList.add("task-btn");
 
   function renderForm() {
-    const form = document.createElement("form");
+    const form = document.querySelector(".task-form");
+    form.classList.add("active");
   }
-  addTaskBtn.addEventListener("click", renderForm);
-  view.append(heading, addTaskBtn);
+
+  const addTaskBtns = document.querySelectorAll(".task-btn");
+  addTaskBtns.forEach((btn) => addEventListener("click", renderForm));
+  view.append(heading, btn);
 }
