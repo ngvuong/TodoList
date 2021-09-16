bd31fc10c22f6dfd79627217bb6fcc208b570ed1
ngvuong <vminguyen@ucdavis.edu>
2021-09-16 01:41:53 -0700

    Update tasks view group by date logic

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import { todayView } from "./today-view";
 import { tasksView } from "./tasks-view";
 import { storeTask } from "./storage";
 
-const task1 = Task("task1", "test", "2021-09-15", "!!!", "project1");
-const task2 = Task("task2", "test", "2021-09-15", "!!!", "project1");
-storeTask.store(task1, task2);
+(function View() {
+  const task1 = Task("task1", "test", "2021-09-15", "!!!", "project1");
+  const task2 = Task("task2", "test", "2021-09-16", "!!!", "project1");
+  storeTask.store(task1, task2);
 
-todayView();
-tasksView();
+  const navBtns = document.querySelectorAll(".nav-btn");
+  navBtns.forEach((btn) => btn.addEventListener("click", renderView));
+
+  function renderView() {
+    const page = 
+  }
+  todayView();
+  tasksView();
+})();
--- src/tasks-view.js
+++ src/tasks-view.js
@@ -4,11 +4,24 @@ export function tasksView() {
   const dates = [...new Set(storeTask.tasks.map((task) => task.date))];
   const tasks = storeTask.tasks;
   const view = document.querySelector(".view");
-  for (const date of dates) {
+
+  const tasksByDate = tasks.reduce((acc, task) => {
+    if (acc[task.date]) {
+      acc[task.date].push(task);
+    } else acc[task.date] = [task];
+
+    return acc;
+  }, {});
+
+  console.log(tasksByDate);
+
+  for (const date in tasksByDate) {
     const group = document.createElement("div");
-    tasks.forEach((task) => {
-      if (task.date === date) {
-      }
+    tasksByDate[date].forEach((task) => {
+      const taskItem = document.createElement("div");
+      group.append(taskItem);
     });
+    view.append(group);
   }
+  console.log(view.innerHTML);
 }
